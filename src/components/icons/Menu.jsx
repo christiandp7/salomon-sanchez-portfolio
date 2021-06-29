96b884437cca8e742f3ef887d17e551eeaf52698
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-28 22:35:16 -0400

    :sparkles: Header responsive done!
    Add Menu icon, refactor header with grids

---
+++ src/components/icons/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Menu = ({ props }) => (
+	<svg
+		width="32"
+		height="32"
+		viewBox="0 0 32 32"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		{...props}>
+		<path
+			d="M5.33333 24H14.6667M5.33333 8H26.6667H5.33333ZM5.33333 16H26.6667H5.33333Z"
+			stroke="#00BAE2"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+)
+
+export default Menu
--- src/components/ui/Header/Header.jsx
+++ src/components/ui/Header/Header.jsx
@@ -1,67 +1,82 @@
 import React from 'react'
 import cx from 'classnames'
 import Link from 'next/link'
-import Logo from '@components/icons/Logo'
 import Container from '@components/ui/Container'
 import s from './Header.module.css'
+// icons
 import { YoutubeIcon } from '@components/icons/Youtube'
 import Twitter from '@components/icons/Twitter'
 import GitHub from '@components/icons/GitHub'
+import Logo from '@components/icons/Logo'
+import Menu from '@components/icons/Menu'
 
 const Header = () => {
 	return (
 		<nav className={s.root}>
 			<Container>
 				<div className={s.headerWrapper}>
-					<div className={s.logo}>
-						<Link href="/">
-							<Logo />
-						</Link>
+					<div className="flex-start-center w-1/6 md:hidden">
+						<button className={s.menuButton}>
+							<Menu />
+						</button>
 					</div>
-					<ul className={s.navbar}>
-						<li>
-							<a href="/" className={cx(s.link, s.active)}>
-								Home
-							</a>
-						</li>
-						<li>
-							<a href="#" className={s.link}>
-								Skills
-							</a>
-						</li>
-						<li>
-							<a href="#" className={s.link}>
-								Portfolio
-							</a>
-						</li>
-						<li>
-							<a href="#" className={s.link}>
-								Blog
-							</a>
-						</li>
-						<li>
-							<a href="#" className={s.link}>
-								Contact
-							</a>
-						</li>
-					</ul>
-					<ul className={s.toolbar}>
-						<li>
-							<a href="#" className={s.socialLink}>
-								<YoutubeIcon />
-							</a>
-						</li>
-						<li>
-							<a href="#" className={s.socialLink}>
-								<Twitter />
-							</a>
-						</li>
-						<li>
-							<a href="#" className={s.socialLink}>
-								<GitHub />
-							</a>
-						</li>
-					</ul>
+
+					<div className="flex justify-center md:justify-start items-center w-4/6 md:w-1/6">
+						<div className={s.logo}>
+							<Link href="/">
+								<Logo />
+							</Link>
+						</div>
+					</div>
+
+					<div className="flex-center hidden md:block w-4/6">
+						<ul className={s.navbar}>
+							<li>
+								<a href="/" className={cx(s.link, s.active)}>
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="#" className={s.link}>
+									Skills
+								</a>
+							</li>
+							<li>
+								<a href="#" className={s.link}>
+									Portfolio
+								</a>
+							</li>
+							<li>
+								<a href="#" className={s.link}>
+									Blog
+								</a>
+							</li>
+							<li>
+								<a href="#" className={s.link}>
+									Contact
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div className=" flex-center w-1/6">
+						<ul className={s.toolbar}>
+							<li>
+								<a href="#" className={cx(s.socialLink, s.youtubeIcon)}>
+									<YoutubeIcon />
+								</a>
+							</li>
+							<li>
+								<a href="#" className={s.socialLink}>
+									<Twitter />
+								</a>
+							</li>
+							<li>
+								<a href="#" className={s.socialLink}>
+									<GitHub />
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</Container>
 		</nav>
